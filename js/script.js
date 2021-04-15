837252539483e4112d50b4589c084d176823bfd3
Gabriela92b <82203384+Gabriela92b@users.noreply.github.com>
2021-04-15 19:16:19 +0200

    proximos productos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let main1 = document.querySelector(".main-principal")
 let main2 = document.querySelector(".descripcion-productos")
+let main4 = document.querySelector(".proximos-productos")
 
 function cambiarPantalla(){
     if(main1.style.display == "block"){
@@ -7,6 +8,9 @@ function cambiarPantalla(){
     }
 if(main2.style.display == "none"){
     main2.style.display = "block"
+}
+if(main4.style.display == "block"){
+    main4.style.display = "none"
 }
 }
 
@@ -17,4 +21,16 @@ function cambiarPantallaHome(){
 if(main1.style.display == "none"){
     main1.style.display = "block"
 }
+}    
+
+function cambiarPantallaProxima(){
+    if(main1.style.display == "block"){
+        main1.style.display = "none"
+    }
+    if(main2.style.display == "block"){
+        main2.style.display = "none"
+    }
+if(main4.style.display == "none"){
+    main4.style.display = "block"
 }
+}    
